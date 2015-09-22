b5dbf1cd2ef4f5f40d0941fbc7503a43cd748a58
PHI PHAM <phiph_57@vnu.edu.vn>
2015-09-22 13:55:43 +0700

    update CFG

---
+++ giaiPTB2.js
@@ -0,0 +1,25 @@
+function GiaiPTB2(a,b,c){
+  var nghiem={};
+  if(!isNaN(a) || !isNaN(b) || !isNaN(c)){
+    if(a==0){
+      if(b==0){
+        if(c==0){
+          nghiem.x='R';
+          return nghiem;
+        }
+        return nghiem;
+      }
+      nghiem.x= k-b/c;
+      return nghiem;
+    } else{
+      var delta= b*b - 4*a*c;
+      if(delta<0){
+        return nghiem;
+      }
+      nghiem.x1= (-b + Math.sqrt(delta))/(2*a);
+      nghiem.x2= (-b - Math.sqrt(delta))/(2*a);
+      return nghiem;
+    }
+  }
+  return nghiem;
+}
