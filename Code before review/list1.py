8adb4a61cab7a6d533644735ced2c055ec4392b6
Phong Nt <phongnt.9441@gmail.com>
2015-11-10 20:13:54 +0700

    Update list1.py

--- Code before review/list1.py
+++ Code before review/list1.py
@@ -1,29 +1,16 @@
-#!/usr/bin/python -tt
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
-# Basic list exercises
-# Fill in the code for the functions below. main() is already set up
-# to call the functions with a few different inputs,
-# printing 'OK' when each function is correct.
-# The starter code for each function includes a 'return'
-# which is just a placeholder for your code.
-# It's ok if you do not complete all the functions, and there
-# are some additional functions to try in list2.py.
-
-# A. match_ends
-# Given a list of strings, return the count of the number of
-# strings where the string length is 2 or more and the first
-# and last chars of the string are the same.
-# Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-  # +++your code here+++
-  return
-
+	# A. Given a list of strings, return the count of the number of
+	# strings where the string length is 2 or more and the first
+	# and last chars of the string are the same.
+	# Note: python does not have a ++ operator, but += works.
+	count = 0
+	for n in words:
+		
+		if (len(n)>=2 and n[0]==n[-1]):
+			count+=1
+			print n
+			
+	return count
 
 # B. front_x
 # Given a list of strings, return a list with the strings
@@ -33,9 +20,21 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
+	
   # +++your code here+++
-  return
-
+	listX = []
+	listWithOutX=[]
+	for word in words:
+		if word.startswith('x') :
+			listX.append(word)
+		else:
+			listWithOutX.append(word)
+	for n in listX:
+		listX = sorted(listX)
+	for m in listWithOutX:
+		listWithOutX = sorted(listWithOutX)
+	listSort = listX + listWithOutX
+	return listSort		
 
 
 # C. sort_last
@@ -44,13 +43,19 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
+def MyFn(s):
+	return s[-1]
 def sort_last(tuples):
   # +++your code here+++
-  return
-
-
-# Simple provided test() function used in main() to print
-# what each function returns vs. what it's supposed to return.
+  tupleAS =  sorted(tuples, key=MyFn)
+  
+	
+  return tupleAS	
+		
+			
+  
+  
+	
 def test(got, expected):
   if got == expected:
     prefix = ' OK '
@@ -84,7 +89,8 @@ def main():
        [(3, 1), (1, 2), (2, 3)])
   test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
        [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
-
-
+			
+		
+# This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
-  main()
+  main()		
